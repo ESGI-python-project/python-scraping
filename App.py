4cbabd87a62cbadbb99f98760c3aae97dde5b8a2
wuchin <wuchin1997@gmail.com>
2022-02-27 18:10:05 +0100

    rename file , bug fix

--- App.py
+++ App.py
@@ -1,6 +1,15 @@
-print("premier affichage \n choisir une action :")
-action = input()
+# print("premier affichage \n choisir une action :")
+# action = input()
+#
+# while(action != "0"):
+#     print(f"affichage dépendant de l'action {action}")
+#     action = input()
 
-while(action != "0"):
-    print(f"affichage dépendant de l'action {action}")
-    action = input()
+#import RestoManager
+import Scraping
+
+resto='bichette'
+
+res = Scraping.get_resto_avis(resto)
+
+print(res)
---
+++ Scraping.py
@@ -0,0 +1,56 @@
+import requests
+import json
+from bs4 import BeautifulSoup
+import re
+
+
+def request_page_html_content(params):
+    headers = {
+        "User-Agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36 OPR/83.0.4254.46"
+    }
+
+    html_content = requests.get(f'https://www.google.com/search?q=', headers=headers, params=params).text
+
+    return html_content
+
+
+def get_resto_research_infos(nom_resto):
+    # resto ="resto+plein+soleil"
+    params = {'q': 'resto ' + nom_resto}
+
+    html_content = request_page_html_content(params)
+    return html_content
+
+
+def filtre_data(html_content):
+    soup_content = BeautifulSoup(html_content, 'html.parser')
+    search_data = soup_content.find('div', {'id': 'search'})
+
+    data_res = search_data.find_all('div', {'class': 'g'})
+
+    resto_note = list()
+
+    for one_resultat in data_res:
+        list_infos = one_resultat.find('div', {'class': 'fG8Fp', 'class': 'uo4vr'})
+        url = one_resultat.find('a').attrs['href']
+        if list_infos:  # avoid the search without nots
+            if "Note" in str(list_infos):
+                noteElement = list_infos.find('span', text=re.compile("Note"))
+                if noteElement:
+                    note = noteElement.getText()
+                    site_name = url.split('.')[1]
+                    if "/" not in note:
+                        note = note + '/5'
+                    attr, note_score = note.split(':')
+                    data_note = {site_name: {attr: note_score}}
+                    resto_note.append(data_note)
+
+    return resto_note
+
+
+def get_resto_avis(nom_resto):
+    html_content = get_resto_research_infos(nom_resto)
+    data = filtre_data(html_content)
+
+    return data
